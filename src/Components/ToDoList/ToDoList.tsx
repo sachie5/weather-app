854f5b6f771a9804d222419c4aa44640b0b48fb9
sachie5 <sachamccormack@gmail.com>
2024-02-28 15:52:41 +0000

    added typograpy and variables styling

--- src/Components/ToDoList/ToDoList.tsx
+++ src/Components/ToDoList/ToDoList.tsx
@@ -1,25 +1,44 @@
 import List from "../List/List";
 import Textbox from "../Textbox/Textbox";
-import { ChangeEventHandler,  MouseEventHandler } from "react";
+import { ChangeEventHandler, MouseEventHandler } from "react";
 
 type ToDoListProps = {
-    entry: string;
-    items: string[];
-    handleListButtonClick: MouseEventHandler<HTMLButtonElement>;
-    handleListItemButtonClick: (id: number) => void;
-    handleChange: ChangeEventHandler<HTMLInputElement>;
-    handleCheckChange: (id: number) => void;
-    checkedItems: boolean[];
-}
+  entry: string;
+  items: string[];
+  handleListButtonClick: MouseEventHandler<HTMLButtonElement>;
+  handleListItemButtonClick: (id: number) => void;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+  handleCheckChange: (id: number) => void;
+  checkedItems: boolean[];
+};
 
-const ToDoList = ({ entry, items,  handleListButtonClick, handleListItemButtonClick, handleChange, handleCheckChange, checkedItems }: ToDoListProps) => {
-
-    return (
-    <section>
-    <Textbox name="todo-list" placeholder="Add your task here..." entry={entry} handleListButtonClick={handleListButtonClick} handleChange={handleChange} />
-    <List items={items} handleListItemButtonClick={handleListItemButtonClick} handleCheckChange={handleCheckChange} checked={checkedItems} />
+const ToDoList = ({
+  entry,
+  items,
+  handleListButtonClick,
+  handleListItemButtonClick,
+  handleChange,
+  handleCheckChange,
+  checkedItems,
+}: ToDoListProps) => {
+  return (
+    <section className="todo">
+      <h1 className="todo__heading">To-Do List</h1>
+      <Textbox
+        name="todo-list"
+        placeholder="Add your task here..."
+        entry={entry}
+        handleListButtonClick={handleListButtonClick}
+        handleChange={handleChange}
+      />
+      <List
+        items={items}
+        handleListItemButtonClick={handleListItemButtonClick}
+        handleCheckChange={handleCheckChange}
+        checked={checkedItems}
+      />
     </section>
-    )
+  );
 };
 
 export default ToDoList;
